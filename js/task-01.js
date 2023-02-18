2b73672be3ec457efa863f704487c5c89d91ae0b
IrynaBuchma <buchma.iryna2018@gmail.com>
2023-02-18 16:40:43 +0200

    task 1 done

---
+++ js/task-01.js
@@ -0,0 +1,35 @@
+// Task 1
+// V.1
+
+/* const categories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categories.length}`);
+
+// Task 2
+
+const categorieContent = categories.forEach(category => {
+
+    console.log(`Category: ${category.firstElementChild.textContent}`);
+
+    category.lastElementChild.classList.add('sub-item');
+    const subItemArray = document.querySelectorAll('.sub-item');
+    console.log(`Elements: ${subItemArray.length}`);
+    }); */
+
+// Task 1
+// V.2
+/* const body = document.body;
+const firstChild = body.firstElementChild;
+const list = firstChild.nextElementSibling;
+const listChildren = list.children;
+console.log(`Number of categories: ${listChildren.length}`); */
+
+// Task 1 
+// V.3
+
+/* const categories = document.querySelectorAll(".item");
+    let total = 0;
+const categoriesQuantity = categories => {
+    categories.forEach(categorie => total += 1);
+    return total;
+    };
+console.log(`Number of categories: ${categoriesQuantity(categories)}`); */
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulRef = document.querySelector('.id');
+console.log(ulRef);
+
+const liListRef = ingredients.map((name) => {
+  const liRef = document.createElement('li');
+  liRef.textContent = name;
+  return liRef;
+});
+
+console.log(liListRef);
+
+
